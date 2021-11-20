acfee58036712a7ac62e781838ebb2d5bb809480
Luke Davis <Luke180994@gmail.com>
2021-11-20 20:23:24 +0000

    Initial Commit

---
+++ Citymapper app/Citymapper_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  Citymapper_appApp.swift
+//  Citymapper app
+//
+//  Created by Luke Davis on 20/11/2021.
+//
+
+import SwiftUI
+
+@main
+struct Citymapper_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
